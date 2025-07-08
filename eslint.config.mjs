50bfea0a41998afc79399c952daeb6a7a7bb797d
adevinwild <68734155+adevinwild@users.noreply.github.com>
2025-07-08 21:33:19 +0200

    chore: update ESLint configuration to ignore .medusa/ folder and improve formatting in service.ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,12 @@
 import { config as defaultConfig } from '@epic-web/config/eslint'
 
-/** @type {import("eslint").Linter.Config} */
-export default [...defaultConfig]
+/**
+ * @type {import("eslint").Linter.Config}
+ * @description ESLint configuration that ignores .medusa/ folder and focuses on src/
+ */
+export default [
+	{
+		ignores: ['.medusa/**'],
+	},
+	...defaultConfig,
+]
